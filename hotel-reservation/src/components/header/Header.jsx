715114200aed144da4731ee9ac93309fb7d4b65a
andyDevMdg <andyavotiana@gmail.com>
2023-08-02 16:00:38 +0300

    Add book now button but it's a div

--- hotel-reservation/src/components/header/Header.jsx
+++ hotel-reservation/src/components/header/Header.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './header.css';
 
 function Header() {
-  return (
-    <div className='header'>
-        <div className="headerList">
-            <div className="headerListItem">
-                
+    return (
+        <div className='header'>
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem">
+                        <span>BOOK NOW</span>
+                        <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
