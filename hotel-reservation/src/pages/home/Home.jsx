b9c8b29c494deeddec18b7302f08f774c7e4c645
andyDevMdg <andyavotiana@gmail.com>
2023-08-02 15:37:40 +0300

    Create Header components

--- hotel-reservation/src/pages/home/Home.jsx
+++ hotel-reservation/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Navbar from '../../components/navbar/Navbar';
 import './home.css';
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
 
 function Home() {
   return (
     <div>
       <Navbar/>
+      <Header/>
       </div>
   )
 }
