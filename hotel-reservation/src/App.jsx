f92b992cf98d0455b4ce1c6d68b050c742068c7d
andyDevMdg <andyavotiana@gmail.com>
2023-08-02 11:37:05 +0300

    Add more Routes

--- hotel-reservation/src/App.jsx
+++ hotel-reservation/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}/> {/* Quand je suis à la racine '/' afficher l'élement Home*/}
+      <Route path="/hotels" element={<List />}/>
+      <Route path="/hotels/:id" element={<Hotel />}/>
       </Routes>
   );
 }
