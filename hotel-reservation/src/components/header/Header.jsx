6dfd121d26fa508ab4974c02aab04da54965c3ad
andyDevMdg <andyavotiana@gmail.com>
2023-08-02 20:46:41 +0300

    Add header search elements

--- hotel-reservation/src/components/header/Header.jsx
+++ hotel-reservation/src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { faBed } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './header.css';
 
@@ -16,6 +18,22 @@ function Header() {
                 <h1 className='headeTitle'>Bienveue chez Hotel</h1>
                 <p className='headerDesc'>Passez des moments agréables dans nos hotels situés dans tout Madagascar</p>
                 <button className="headerBtn">S'inscrire / Se connecter</button>
+                <div className="headerSearch">
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={ faBed } className='headerIcon' />
+                        <input type="text"
+                            placeholder='Choisissez une destination'
+                            className='headerSearchInput' />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={ faCalendarDays } className='headerIcon' />
+                        <span className='headerSearchText'>date to date</span>
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faBed} className='headerIcon' />
+                        <span className='headerSearchText'>2 adults 2 children 1 room</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
