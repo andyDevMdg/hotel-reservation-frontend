ce1f9ec54430cc02267c34bd3bc5477a23c7eca8
andyDevMdg <andyavotiana@gmail.com>
2023-08-02 11:09:40 +0300

    Add changes to Routes toward Home element

--- hotel-reservation/src/App.jsx
+++ hotel-reservation/src/App.jsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import {} from './pages/Home/Home'
+import { BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />}></Route> {/* Quand je suis à la racine '/' afficher l'élement Home*/}
+      <Route path="/" element={<Home />}/> {/* Quand je suis à la racine '/' afficher l'élement Home*/}
       </Routes>
-  )
+  );
 }
 
 export default App;
