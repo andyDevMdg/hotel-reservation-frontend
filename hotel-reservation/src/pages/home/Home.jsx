e8843806f99d4f4f1af48c528a725852962cbb87
andyDevMdg <andyavotiana@gmail.com>
2023-08-03 15:31:34 +0300

    Create the Promotion component

--- hotel-reservation/src/pages/home/Home.jsx
+++ hotel-reservation/src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './home.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
+import Promotion from '../../components/promotion/Promotion';
 
 function Home() {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <div className="homeContainer">
+        <Promotion/>
+      </div>
       </div>
   )
 }
