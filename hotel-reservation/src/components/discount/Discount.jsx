e4bd6631efbac6b73735ddcce83bfc8f298cf553
andyDevMdg <andyavotiana@gmail.com>
2023-08-03 17:36:50 +0300

    Resize the promotion images

--- hotel-reservation/src/components/discount/Discount.jsx
+++ hotel-reservation/src/components/discount/Discount.jsx
@@ -10,22 +10,19 @@ function Discount() {
         <div className="discountItem">
           <img src={twinPromotion} alt="" className="discountImg" />
           <div className="discountTitle">
-            <h1>Chambre Twin</h1>
-            <h2>6 Chambres</h2>
+            <h3>Chambre Twin</h3>
           </div>
         </div>
         <div className="discountItem">
           <img src={familyPromotion} alt="" className="discountImg" />
           <div className="discountTitle">
-            <h1>Chambre familiale</h1>
-            <h2>5 Chambres</h2>
+            <h3>Chambre familiale</h3>
           </div>
         </div>
         <div className="discountItem">
           <img src={suitePromotion} alt="" className="discountImg" />
           <div className="discountTitle">
-            <h1>Suite</h1>
-            <h2>4 Chambres</h2>
+            <h3>Suite</h3>
           </div>
         </div>
     </div>
