5e6ad264fafc6c2250c1b3909d5835b7ac8c51b3
andyDevMdg <andyavotiana@gmail.com>
2023-08-03 19:55:47 +0300

    Add simple room to the list

--- hotel-reservation/src/components/discount/Discount.jsx
+++ hotel-reservation/src/components/discount/Discount.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './discount.css';
+import simplePromotion from '../../img/simplePromotion.jpg';
 import twinPromotion from '../../img/twinPromotion.jpg';
 import familyPromotion from '../../img/familyPromotion.jpg';
 import suitePromotion from '../../img/suitePromotion.jpg';
@@ -7,24 +8,30 @@ import suitePromotion from '../../img/suitePromotion.jpg';
 function Discount() {
   return (
     <div className='discount'>
-        <div className="discountItem">
-          <img src={twinPromotion} alt="" className="discountImg" />
-          <div className="discountTitle">
-            <h3>Chambre Twin</h3>
-          </div>
+      <div className="discountItem">
+        <img src={simplePromotion} alt="" className="discountImg" />
+        <div className="discountTitle">
+          <h3>Chambre Simple</h3>
         </div>
-        <div className="discountItem">
-          <img src={familyPromotion} alt="" className="discountImg" />
-          <div className="discountTitle">
-            <h3>Chambre familiale</h3>
-          </div>
+      </div>
+      <div className="discountItem">
+        <img src={twinPromotion} alt="" className="discountImg" />
+        <div className="discountTitle">
+          <h3>Chambre Twin</h3>
         </div>
-        <div className="discountItem">
-          <img src={suitePromotion} alt="" className="discountImg" />
-          <div className="discountTitle">
-            <h3>Suite</h3>
-          </div>
+      </div>
+      <div className="discountItem">
+        <img src={familyPromotion} alt="" className="discountImg" />
+        <div className="discountTitle">
+          <h3>Chambre familiale</h3>
         </div>
+      </div>
+      <div className="discountItem">
+        <img src={suitePromotion} alt="" className="discountImg" />
+        <div className="discountTitle">
+          <h3>Suite</h3>
+        </div>
+      </div>
     </div>
   )
 }
