079e7ac255adca2845abf98993aacaefdc396396
andyDevMdg <andyavotiana@gmail.com>
2023-08-03 20:12:03 +0300

    Add styles to the bestRoom items

--- hotel-reservation/src/components/bestRoom/BestRoom.jsx
+++ hotel-reservation/src/components/bestRoom/BestRoom.jsx
@@ -8,9 +8,9 @@ function BestRoom() {
     return (
         <div className='bestRoom'>
             <div className="bestRoomItem">
-                <img src={twin} alt="" className="bestRoom" />
-                <span className="bestRoomName"></span>
-                <span className="bestRoomCity"></span>
+                <img src={twin} alt="" className="bestRoomImg" />
+                <span className="bestRoomName">Chambre Twin</span>
+                <span className="bestRoomCity">Toamasina</span>
                 <span className="bestRoomPrice">A partir de 40.000ar</span>
                 <div className="bestRoomRating">
                     <button>4.5</button>
@@ -18,9 +18,9 @@ function BestRoom() {
                 </div>
             </div>
             <div className="bestRoomItem">
-                <img src={family} alt="" className="bestRoom" />
-                <span className="bestRoomName"></span>
-                <span className="bestRoomCity"></span>
+                <img src={family} alt="" className="bestRoomImg" />
+                <span className="bestRoomName">Chambre Familiale</span>
+                <span className="bestRoomCity">Majunga</span>
                 <span className="bestRoomPrice">A partir de 90.000ar</span>
                 <div className="bestRoomRating">
                     <button>4.5</button>
@@ -28,9 +28,9 @@ function BestRoom() {
                 </div>
             </div>
             <div className="bestRoomItem">
-                <img src={suite} alt="" className="bestRoom" />
-                <span className="bestRoomName"></span>
-                <span className="bestRoomCity"></span>
+                <img src={suite} alt="" className="bestRoomImg" />
+                <span className="bestRoomName">Suite</span>
+                <span className="bestRoomCity">Nosy Be</span>
                 <span className="bestRoomPrice">A partir de 150.000ar</span>
                 <div className="bestRoomRating">
                     <button>4.5</button>
