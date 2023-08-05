671566c191d4c20f9f079a247fe7a80be69d5aee
andyDevMdg <andyavotiana@gmail.com>
2023-08-05 11:18:53 +0300

    Add div with classname for search result

--- hotel-reservation/src/pages/list/List.jsx
+++ hotel-reservation/src/pages/list/List.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
+import './list.css';
 
 function List() {
   return (
     <div>
       <Navbar/>
       <Header type="list"/>
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="listSearchTitle">Recherche</h1>
+            <div className="listResult"></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
