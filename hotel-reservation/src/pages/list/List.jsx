b820afbe3e1d4b3f5ce44422904c5058b89c10ad
andyDevMdg <andyavotiana@gmail.com>
2023-08-05 13:27:44 +0300

    Add CSS block to the search result section

--- hotel-reservation/src/pages/list/List.jsx
+++ hotel-reservation/src/pages/list/List.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 import './list.css';
+import { format } from 'date-fns';
 import { useLocation } from 'react-router';
+
 
 function List() {
 
   const location =  useLocation()
+  const [destination,setDestination] = useState(location.state.destination)
+  const [date,setDate] = useState(location.state.date)
+  const [options,setOptions] = useState(location.state.options)
 
   return (
     <div>
@@ -18,11 +23,11 @@ function List() {
             <h1 className="listSearchTitle">Recherche</h1>
             <div className="listItem">
               <label>Destination</label>
-              <input type="text" />
+              <input type="text" placeholder={destination}/>
             </div>
             <div className="listItem">
               <label>Dates selectionnées</label>
-              <input type="text" />
+              <span>{`${format(date[0].startDate, "dd/MM/yyyy")} à ${format(date[0].endDate, "dd/MM/yyyy")}`}</span>
             </div>
             <div className="listResult"></div>
           </div>
