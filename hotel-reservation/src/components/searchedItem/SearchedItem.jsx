8a87b0870a16b9ea5495dee42a5bdc11faef7d8f
andyDevMdg <andyavotiana@gmail.com>
2023-08-05 15:06:54 +0300

    Add rating details & see availability button to searced item

--- hotel-reservation/src/components/searchedItem/SearchedItem.jsx
+++ hotel-reservation/src/components/searchedItem/SearchedItem.jsx
@@ -3,28 +3,38 @@ import './searchedItem.css'
 import img1 from '../../img/sbed01.jpg'
 
 function SearchedItem() {
-  return (
-    <div className='searchedItem'>
-        <img src={img1} alt="Picture" 
-        className='searchedItemImg'/>
-        <div className="siDesc">
-        <h1 className="siTitle">Tower Street Apartments</h1>
-        <span className="siDistance">500m from center</span>
-        <span className="siTaxiOp">Free airport taxi</span>
-        <span className="siSubtitle">
-          Studio Apartment with Air conditioning
-        </span>
-        <span className="siFeatures">
-          Entire studio • 1 bathroom • 21m² 1 full bed
-        </span>
-        <span className="siCancelOp">Free cancellation </span>
-        <span className="siCancelOpSubtitle">
-          You can cancel later, so lock in this great price today!
-        </span>
+    return (
+        <div className='searchedItem'>
+            <img src={img1} alt="Picture"
+                className='searchedItemImg' />
+            <div className="siDesc">
+                <h1 className="siTitle">Tower Street Apartments</h1>
+                <span className="siDistance">500m from center</span>
+                <span className="siTaxiOp">Free airport taxi</span>
+                <span className="siSubtitle">
+                    Studio Apartment with Air conditioning
+                </span>
+                <span className="siFeatures">
+                    Entire studio • 1 bathroom • 21m² 1 full bed
+                </span>
+                <span className="siCancelOp">Free cancellation </span>
+                <span className="siCancelOpSubtitle">
+                    You can cancel later, so lock in this great price today!
+                </span>
+            </div>
+            <div className="siDetails">
+                <div className="siRating">
+                    <span>Excellent</span>
+                    <button>8.9</button>
+                </div>
+                <div className="siDetailTexts">
+                    <span className="siPrice">$112</span>
+                    <span className="siTaxOp">Includes taxes and fees</span>
+                    <button className="siCheckButton">See availability</button>
+                </div>
+            </div>
         </div>
-        <div className="siDetails">details</div>
-    </div>
-  )
+    )
 }
 
 export default SearchedItem
