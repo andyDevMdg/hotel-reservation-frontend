e46b61bb106643ee330e2b9fc5120315ac30889c
andyDevMdg <andyavotiana@gmail.com>
2023-08-05 15:01:49 +0300

    Add text description & styles for the searched item

--- hotel-reservation/src/components/searchedItem/SearchedItem.jsx
+++ hotel-reservation/src/components/searchedItem/SearchedItem.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import './searchedItem.css'
-import img1 from '../../img'
+import img1 from '../../img/sbed01.jpg'
 
 function SearchedItem() {
   return (
     <div className='searchedItem'>
-        <img src="" alt="Picture" 
+        <img src={img1} alt="Picture" 
         className='searchedItemImg'/>
+        <div className="siDesc">
+        <h1 className="siTitle">Tower Street Apartments</h1>
+        <span className="siDistance">500m from center</span>
+        <span className="siTaxiOp">Free airport taxi</span>
+        <span className="siSubtitle">
+          Studio Apartment with Air conditioning
+        </span>
+        <span className="siFeatures">
+          Entire studio • 1 bathroom • 21m² 1 full bed
+        </span>
+        <span className="siCancelOp">Free cancellation </span>
+        <span className="siCancelOpSubtitle">
+          You can cancel later, so lock in this great price today!
+        </span>
+        </div>
+        <div className="siDetails">details</div>
     </div>
   )
 }
