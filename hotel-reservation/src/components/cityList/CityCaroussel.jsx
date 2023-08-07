1ba7aac34f2470be75d37a69676d161c89cbb1b7
andyDevMdg <andyavotiana@gmail.com>
2023-08-07 13:23:45 +0300

    Add carousel effect for city list

---
+++ hotel-reservation/src/components/cityList/CityCaroussel.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import nosyBe from '../../img/NosyBe.jpg';
+import antsiranana from '../../img/Antsiranana.jpg';
+import majunga from '../../img/Majunga.jpg';
+import toamasina from '../../img/Toamasina.jpg';
+import antananarivo from '../../img/Antananarivo.jpg';
+import fianarantsoa from '../../img/Fianarantsoa.jpg';
+import tulear from '../../img/Tulear.jpg';
+
+function CityCaroussel() {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
+    return (
+        <div>
+            <Carousel responsive={responsive}>
+                <div className="cityListItem">
+                    <img src={nosyBe} alt="" className="cityListImg" />
+                    <div className="cityListTitle">
+                        <h3>Nosy Be</h3>
+                    </div>
+                </div>
+                <div className="cityListItem">
+                    <img src={antsiranana} alt="" className="cityListImg" />
+                    <div className="cityListTitle">
+                        <h3>Antsiranana</h3>
+                    </div>
+                </div>
+                <div className="cityListItem">
+                    <img src={majunga} alt="" className="cityListImg" />
+                    <div className="cityListTitle">
+                        <h3>Majunga</h3>
+                    </div>
+                </div>
+                <div className="cityListItem">
+                    <img src={toamasina} alt="" className="cityListImg" />
+                    <div className="cityListTitle">
+                        <h3>Toamasina</h3>
+                    </div>
+                </div>
+                <div className="cityListItem">
+                    <img src={antananarivo} alt="" className="cityListImg" />
+                    <div className="cityListTitle">
+                        <h3>Antananarivo</h3>
+                    </div>
+                </div>
+                <div className="cityListItem">
+                    <img src={fianarantsoa} alt="" className="cityListImg" />
+                    <div className="cityListTitle">
+                        <h3>Fianarantsoa</h3>
+                    </div>
+                </div>
+                <div className="cityListItem">
+                    <img src={tulear} alt="" className="cityListImg" />
+                    <div className="cityListTitle">
+                        <h3>Tulear</h3>
+                    </div>
+                </div>
+            </Carousel>
+        </div>
+    )
+}
+
+export default CityCaroussel
--- hotel-reservation/src/components/cityList/CityList.jsx
+++ hotel-reservation/src/components/cityList/CityList.jsx
@@ -1,60 +1,16 @@
 import React from 'react';
 import './cityList.css';
-import nosyBe from '../../img/NosyBe.jpg';
-import antsiranana from '../../img/Antsiranana.jpg';
-import majunga from '../../img/Majunga.jpg';
-import toamasina from '../../img/Toamasina.jpg';
-import antananarivo from '../../img/Antananarivo.jpg';
-import fianarantsoa from '../../img/Fianarantsoa.jpg';
-import tulear from '../../img/Tulear.jpg';
+
+import CityCaroussel from './CityCaroussel';
 
 function CityList() {
-  return (
-    <div className='cityList'>
-        <div className="cityListItem">
-            <img src={nosyBe} alt="" className="cityListImg" />
-            <div className="cityListTitle">
-                <h3>Nosy Be</h3>
-            </div>
+
+    return (
+        <div className='cityList'>
+            <CityCaroussel />
+            
         </div>
-        <div className="cityListItem">
-            <img src={antsiranana} alt="" className="cityListImg" />
-            <div className="cityListTitle">
-                <h3>Antsiranana</h3>
-            </div>
-        </div>
-        <div className="cityListItem">
-            <img src={majunga} alt="" className="cityListImg" />
-            <div className="cityListTitle">
-                <h3>Majunga</h3>
-            </div>
-        </div>
-        <div className="cityListItem">
-            <img src={toamasina} alt="" className="cityListImg" />
-            <div className="cityListTitle">
-                <h3>Toamasina</h3>
-            </div>
-        </div>
-        <div className="cityListItem">
-            <img src={antananarivo} alt="" className="cityListImg" />
-            <div className="cityListTitle">
-                <h3>Antananarivo</h3>
-            </div>
-        </div>
-        <div className="cityListItem">
-            <img src={fianarantsoa} alt="" className="cityListImg" />
-            <div className="cityListTitle">
-                <h3>Fianarantsoa</h3>
-            </div>
-        </div>
-        <div className="cityListItem">
-            <img src={tulear} alt="" className="cityListImg" />
-            <div className="cityListTitle">
-                <h3>Tulear</h3>
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default CityList
