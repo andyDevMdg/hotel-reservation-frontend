5dd99ddec556a78c838eca2890068bc96611a57b
andyDevMdg <andyavotiana@gmail.com>
2023-08-07 09:57:29 +0300

    Add routes to login ang signup pages

--- hotel-reservation/src/App.jsx
+++ hotel-reservation/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import Home from './pages/home/Home';
 import List from './pages/list/List';
 import Hotel from './pages/hotel/Hotel';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route path="/" element={<Home />}/> {/* Quand je suis à la racine '/' afficher l'élement Home*/}
       <Route path="/hotels" element={<List />}/>
       <Route path="/hotels/:id" element={<Hotel />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/register" element={<Register />}/>
       </Routes>
   );
 }
--- hotel-reservation/src/components/navbar/Navbar.jsx
+++ hotel-reservation/src/components/navbar/Navbar.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import './navbar.css';
+import { useNavigate } from 'react-router';
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleRegister = () => {
+    navigate("/register")
+  }
+
+  const handleLogin = () => {
+    navigate("/login")
+  }
+
   return (
     <div className='navbar'>
         <div className="navContainer">
             <span className="logo">Hotel</span>
             <div className="navItem">
-                <button className="navButtonBlack">S'inscrire</button>
-                <button className="navButtonWhite">Se connecter</button>
+                <button className="navButtonBlack" 
+                onClick={handleRegister}>S'inscrire</button>
+                <button className="navButtonWhite" 
+                onClick={handleLogin}>Se connecter</button>
             </div>
         </div>
     </div>
