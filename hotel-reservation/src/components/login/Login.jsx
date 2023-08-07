42eb3cf04d7c935f4927df5ccf58d675f669459f
andyDevMdg <andyavotiana@gmail.com>
2023-08-07 19:50:55 +0300

    Add login component & starting CSS styles

--- hotel-reservation/src/components/login/Login.jsx
+++ hotel-reservation/src/components/login/Login.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import './login.css'
+
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <div className="loginContainer">
+        <div className="loginBody">
+          <div className="hotelName">
+            <h1>GRAND HOTEL
+            </h1>
+          </div>
+          <div className="loginInputContainer">
+            <h2>Se connecter à votre compte</h2>
+            <div className="loginInput">
+              <input type="email" placeholder='E-mail' className='inputTag' />
+              <input type="password" className='inputTag' placeholder='Mot de passe' />
+            </div>
+            <a href=""> Mot de passe oublié ?</a>
+            <button>Se connecter</button>
+            <p className='separator'>ou</p>
+            <button>S'inscrire</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
